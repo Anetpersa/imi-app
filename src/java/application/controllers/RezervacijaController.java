f27f69ee1a9de036e187f8faa6135441cf508781
Ana Stancic <anet.persa@gmail.com>
2019-01-12 22:32:38 +0100

    Added test comment

--- src/java/application/controllers/RezervacijaController.java
+++ src/java/application/controllers/RezervacijaController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class RezervacijaController {
 
+    // TODO: Ovo da popravis jesi cula!
     @Autowired
     private RezervacijaService rezervacijaService;
 
